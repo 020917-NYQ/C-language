53c91a3df05c0dd0c5e238417aea8049b2dfaadf
niu-yingquan <2026371232@qq.com>
2026-01-07 17:33:24 +0800

    初识C语言 (4)

---
+++ test_26_01_05/test_26_01_05/add.c
@@ -0,0 +1,9 @@
+﻿int add = 2026; //全局变量，具有外部链接属性
+static int val = 2026; //用static修饰全局变量
+
+//函数同样具有外部链接属性，加上static修饰后，变成了内部链接属性
+//这样该函数只能在本文件中使用，其他外部文件中无法使用
+static int Add(int x, int y)
+{
+	return x + y;
+}
