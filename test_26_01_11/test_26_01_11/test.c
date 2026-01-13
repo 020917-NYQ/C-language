46c1ec1d590344f9f1aceba97e19eff0bdb992f3
niu-yingquan <2026371232@qq.com>
2026-01-13 20:11:25 +0800

    函数

--- test_26_01_11/test_26_01_11/test.c
+++ test_26_01_11/test_26_01_11/test.c
@@ -1,6 +1,7 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 ////strcpy()函数
 ////函数头文件：<string.h>
 ////函数声明：char* strcpy (char* destination, const char* source);
@@ -85,25 +86,101 @@
 //}
 
 
-//写一个函数判断一个数字是否是素数
-//素数是只能被1和本身整除的数
-int is_prime(int n)
-{
-	if ((n % 2 == 0)) //偶数一定不是素数
-		return 0;
+////写一个函数判断一个数字是否是素数
+//int is_prime_num(int n)
+//{
+//	if (n % 2 == 0 && n != 2) //非2偶数一定不是素数
+//		return 0;
+//
+//	for (int i = 2; i <= sqrt(n); i++)
+//		if (n % i == 0) //能整除一定不是素数
+//			return 0;
+//
+//	return 1; //不是非2偶数也不能整除一定是素数
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	printf("请输入一个正整数，用来判断是否是素数：");
+//	scanf("%d", &n);
+//
+//	if (is_prime_num(n)) //返回1是素数，返回0不是素数
+//		printf("%d是素数\n", n);
+//	else
+//		printf("%d不是素数\n", n);
+//	return 0;
+//}
 
-	
-}
 
-int main()
-{
-	int n = 0;
-	printf("请输入一个正整数，用来判断是否是素数：");
-	scanf("%d", &n);
-	int ret = is_prime(n); //用于判断n是否是素数，返回1是素数，返回0不是
-	if (ret == 1)
-		printf("%d是素数\n", n);
-	else
-		printf("%d不是素数\n", n);
-	return 0;
-}
+////写一个函数判断某一年是否是闰年
+//int is_ryear(int year)
+//{
+//	return ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0);
+//}
+//
+//int main()
+//{
+//	int year = 0;
+//	scanf("%d", &year);
+//	if (is_ryear(year))
+//		printf("%d年是闰年\n", year);
+//	else
+//		printf("%d年不是闰年\n", year);
+//	return 0;
+//}
+
+
+////写一个函数实现整型有序数组的二分查找
+//int binary_search(int arr[], int k, int sz)
+//{
+//	int left = 0, right = sz - 1; //左右下标
+//	while (left <= right)
+//	{
+//		int mid = left + (right - left) / 2; //中间元素下标
+//		if (k > arr[mid])
+//			left = mid + 1;
+//		else if (k < arr[mid])
+//			right = mid - 1;
+//		else
+//			return mid; //找到返回下标
+//	}
+//	return -1; //未找到返回-1
+//}
+//
+//int main()
+//{
+//	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//	int k = 0; //待查找的数字
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	scanf("%d", &k);
+//	
+//	int ret = binary_search(arr, k, sz); //二分查找
+//	if (ret == -1)
+//		printf("未找到\n");
+//	else
+//		printf("找到了，下标是%d\n", ret);
+//	return 0;
+//}
+
+
+////写一个函数每次执行都会将num的值加1
+//void Add(int* p)
+//{
+//	*p = *p + 1;
+//}
+//
+//int main()
+//{
+//	int num = 0, count = 0;
+//	printf("输入执行次数：");
+//	scanf("%d", &count);
+//	for (int i = 0; i < count; i++)
+//	{
+//		Add(&num);
+//		printf("%d\n", num);
+//	}
+//	return 0;
+//}
+
+
